4af86fe1dc19ee908aafe1982916abd68bfb5865
James Hopkins <jameshopkins0709@gmail.com>
2014-09-03 17:33:59 +0100

    Initial test setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,29 +35,42 @@ module.exports = function(grunt) {
                     "dist/on-screen-keyboard.min.js": [ "dist/on-screen-keyboard.js" ]
                 }
             }
+        },
+
+        testem: {
+            all: {
+              src: [
+                'test/testem.json'
+              ],
+            },
+            options: {
+                launch_in_ci : ['firefox']
+            }
         }
 
     });
 
     // load the tasks
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks("grunt-contrib-copy");
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks("grunt-contrib-clean");
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks("grunt-contrib-uglify");
 
-    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+
+    grunt.loadNpmTasks('grunt-testem');
 
     grunt.registerTask(
-        'build',
-        'Compiles all of the assets and copies the files to the build directory.',
-        [ 'clean', 'jshint', 'copy', 'uglify' ]
+        "build",
+        "Compiles all of the assets and copies the files to the build directory.",
+        [ "clean", "jshint", "copy", "uglify", "testem" ]
     );
 
     grunt.registerTask(
-        'default',
-        'Watches the project for changes, automatically builds them and runs a server.',
-        [ 'build' ]
+        "default",
+        "Watches the project for changes, automatically builds them and runs a server.",
+        [ "build" ]
     );
 
 };
---
+++ test/hello.js
@@ -0,0 +1,4 @@
+var expect = chai.expect;
+it("should be attributed to a valid 'look' category", function () {
+    expect('ss').to.be.a('string');
+});
