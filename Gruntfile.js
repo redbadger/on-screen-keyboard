8a1eeaae1fac645fb324603499d0c745a6c844a4
James Hopkins <jameshopkins0709@gmail.com>
2014-09-04 09:15:04 +0100

    Task descriptions were inaccurate

--- Gruntfile.js
+++ Gruntfile.js
@@ -69,7 +69,7 @@ module.exports = function(grunt) {
 
     grunt.registerTask(
         "default",
-        "Watches the project for changes, automatically builds them and runs a server.",
+        "Alias for build task",
         [ "build" ]
     );
 
